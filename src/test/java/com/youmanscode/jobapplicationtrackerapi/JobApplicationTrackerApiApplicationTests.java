228936995e162d7b4c2389032d3278c9506ea809
Michael Youmans <myoumans2013@gmail.com>
2026-06-06 14:28:44 -0400

    Create basic project structure for API

---
+++ src/test/java/com/youmanscode/jobapplicationtrackerapi/JobApplicationTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youmanscode.jobapplicationtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

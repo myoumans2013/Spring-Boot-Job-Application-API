eccdaf7a28f48bf7a773adc536cb8d6b360d7985
Michael Youmans <myoumans2013@gmail.com>
2026-06-11 18:07:25 -0400

    Delete unnecessary imports

--- src/main/java/com/youmanscode/jobapplicationtrackerapi/application/InterviewRepository.java
+++ src/main/java/com/youmanscode/jobapplicationtrackerapi/application/InterviewRepository.java
@@ -3,14 +3,13 @@ package com.youmanscode.jobapplicationtrackerapi.application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
 
-    public List<Interview> findByJobApplication_Id(Long id);
+    List<Interview> findByJobApplication_Id(Long id);
 
     int countInterviewsByJobApplicationId(Long id);
 

e0bf50de0286fc271d34803f7a11acf185c99ddc
Michael Youmans <myoumans2013@gmail.com>
2026-08-04 14:21:01 -0400

    Update Cors Config

--- src/main/java/com/youmanscode/jobapplicationtrackerapi/CorsConfig.java
+++ src/main/java/com/youmanscode/jobapplicationtrackerapi/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://job-application-tracker-frontend-2021.onrender.com"
+                        "https://job-application-tracker-frontend-2o21.onrender.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");

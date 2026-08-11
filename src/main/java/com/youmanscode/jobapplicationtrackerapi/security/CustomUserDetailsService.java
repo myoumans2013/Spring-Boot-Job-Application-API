d4151c8876eadbd656b676a9f7164406303a11ca
Michael Youmans <myoumans2013@gmail.com>
2026-08-11 18:55:45 -0400

    Implement custom user details and service

--- src/main/java/com/youmanscode/jobapplicationtrackerapi/security/CustomUserDetailsService.java
+++ src/main/java/com/youmanscode/jobapplicationtrackerapi/security/CustomUserDetailsService.java
@@ -2,21 +2,25 @@ package com.youmanscode.jobapplicationtrackerapi.security;
 
 import com.youmanscode.jobapplicationtrackerapi.entity.User;
 import com.youmanscode.jobapplicationtrackerapi.exceptionHandling.UsernameNotFoundException;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private final CustomUserRepository customUserRepository;
 
     public CustomUserDetailsService(CustomUserRepository customUserRepository) {
         this.customUserRepository = customUserRepository;
     }
 
-    public User findUserByUsername(String username) {
-        return customUserRepository.findUserByUsername(username)
-                .orElseThrow(()
-                        -> new UsernameNotFoundException("Cannot find user"));
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String username)
+            throws org.springframework.security.core.userdetails.UsernameNotFoundException {
+        User user = customUserRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Cannot find username."));
+
+        return new CustomUserDetails(user);
     }
 }

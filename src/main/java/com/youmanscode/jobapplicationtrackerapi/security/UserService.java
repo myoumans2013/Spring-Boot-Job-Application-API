ad3fa83c27a3f0160161e1e51990d615896898fe
Michael Youmans <myoumans2013@gmail.com>
2026-08-13 17:59:07 -0400

    Create method for saving new user

---
+++ src/main/java/com/youmanscode/jobapplicationtrackerapi/security/UserService.java
@@ -0,0 +1,22 @@
+package com.youmanscode.jobapplicationtrackerapi.security;
+
+import com.youmanscode.jobapplicationtrackerapi.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final CustomUserRepository customUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(CustomUserRepository customUserRepository, PasswordEncoder passwordEncoder) {
+        this.customUserRepository = customUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User saveNewUser(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        return customUserRepository.save(user);
+    }
+}
